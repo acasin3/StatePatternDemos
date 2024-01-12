0a1dec79088fb60b310fa7c7fd190ec21069847a
Alejandro Casin III <jpgrxyz@gmail.com>
2024-01-12 16:14:08 +0800

    Refactored to use state classes

--- StatePatternInWinformsDemo/Form1.cs
+++ StatePatternInWinformsDemo/Form1.cs
@@ -4,118 +4,53 @@ namespace StatePatternDemo
 {
     public partial class Form1 : Form
     {
-        private enum DemoModule
-        {
-            Users,
-            Roles
-        }
-
-        private DemoModule _demoMod = DemoModule.Users;
+        private IUIState _currentUIState;
+        private IUIState _usersState = new UsersState();
+        private IUIState _rolesState = new RolesState();
 
         public Form1()
         {
             InitializeComponent();
+            _currentUIState = _usersState;
             ListEntities();
         }
 
         private void newToolStripButton_Click(object sender, EventArgs e)
         {
-            AddEntity();
+            _currentUIState.Add();
         }
 
         private void editToolStripButton_Click(object sender, EventArgs e)
         {
-            EditEntity();
+            _currentUIState.Edit();
         }
 
         private void deleteToolStripButton_Click(object sender, EventArgs e)
         {
-            DeleteEntity();
+            _currentUIState.Delete();
         }
 
         private void printToolStripButton_Click(object sender, EventArgs e)
         {
-            PrintEntity();
+            _currentUIState.Print();
         }
 
         private void usersToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            _demoMod = DemoModule.Users;
+            _currentUIState = _usersState;
             ListEntities();
         }
 
         private void rolesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            _demoMod = DemoModule.Roles;
+            _currentUIState = _rolesState;
             ListEntities();
         }
 
         private void ListEntities()
         {
-            switch (_demoMod)
-            {
-                case DemoModule.Users:
-                    this.Text = "Users";
-                    MessageBox.Show("Code to list users goes here.");
-                    break;
-                case DemoModule.Roles:
-                    this.Text = "Roles";
-                    MessageBox.Show("Code to list roles goes here.");
-                    break;
-
-            }
-        }
-
-        private void AddEntity()
-        {
-            switch (_demoMod)
-            {
-                case DemoModule.Users:
-                    MessageBox.Show("Code to add a user goes here.");
-                    break;
-                case DemoModule.Roles:
-                    MessageBox.Show("Code to add a role goes here.");
-                    break;
-            }
-        }
-
-        private void EditEntity()
-        {
-            switch (_demoMod)
-            {
-                case DemoModule.Users:
-                    MessageBox.Show("Code to edit a user goes here.");
-                    break;
-                case DemoModule.Roles:
-                    MessageBox.Show("Code to edit a role goes here.");
-                    break;
-            }
-        }
-
-        private void DeleteEntity()
-        {
-            switch (_demoMod)
-            {
-                case DemoModule.Users:
-                    MessageBox.Show("Code to delete a user goes here.");
-                    break;
-                case DemoModule.Roles:
-                    MessageBox.Show("Code to delete a role goes here.");
-                    break;
-            }
-        }
-
-        private void PrintEntity()
-        {
-            switch (_demoMod)
-            {
-                case DemoModule.Users:
-                    MessageBox.Show("Code to print list of users goes here.");
-                    break;
-                case DemoModule.Roles:
-                    MessageBox.Show("Code to print list of roles goes here.");
-                    break;
-            }
+            this.Text = _currentUIState.Caption;
+            _currentUIState.ListEntities();
         }
     }
 }
