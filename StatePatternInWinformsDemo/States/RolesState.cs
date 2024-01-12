b34eedaba440cd5fe795c9d5ea8e11dbea435154
Alejandro Casin III <jpgrxyz@gmail.com>
2024-01-12 16:04:05 +0800

    Created the state classes

---
+++ StatePatternInWinformsDemo/States/RolesState.cs
@@ -0,0 +1,32 @@
+﻿namespace StatePatternDemo
+{
+    internal class RolesState : IUIState
+    {
+        public string Caption => "Roles";
+
+        public void ListEntities()
+        {
+            MessageBox.Show("Code to list roles goes here.");
+        }
+
+        public void Add()
+        {
+            MessageBox.Show("Code to add a role goes here.");
+        }
+
+        public void Edit()
+        {
+            MessageBox.Show("Code to edit a role goes here.");
+        }
+
+        public void Delete()
+        {
+            MessageBox.Show("Code to delete a role goes here.");
+        }
+
+        public void Print()
+        {
+            MessageBox.Show("Code to print list of roles goes here.");
+        }
+    }
+}
---
+++ StatePatternInWinformsDemo/States/UsersState.cs
@@ -0,0 +1,32 @@
+﻿namespace StatePatternDemo
+{
+    internal class UsersState : IUIState
+    {
+        public string Caption => "Users";
+
+        public void ListEntities()
+        {
+            MessageBox.Show("Code to list users goes here.");
+        }
+
+        public void Add()
+        {
+            MessageBox.Show("Code to add a user goes here.");
+        }
+
+        public void Edit()
+        {
+            MessageBox.Show("Code to edit a user goes here.");
+        }
+
+        public void Delete()
+        {
+            MessageBox.Show("Code to delete a user goes here.");
+        }
+
+        public void Print()
+        {
+            MessageBox.Show("Code to print list of users goes here.");
+        }
+    }
+}
